9e7a550e430fb7553e55e31c4ad7f62cf7e93eca
jackiezhi <1543243086@qq.com>
2016-04-15 14:31:27 +0800

    modify locksupport demo

--- src/main/java/com/zhi/juc/LockSupportDemo.java
+++ src/main/java/com/zhi/juc/LockSupportDemo.java
@@ -4,11 +4,16 @@ import java.util.concurrent.locks.LockSupport;
 
 /**
  * Created by jackiezhi on 2016/4/14.
- * LockSupport 提供的方法主要是park/unpark 以及相关的重载的方法。底层使用到了native方法实现。
+ * LockSupportDemo2 提供的方法主要是park/unpark 以及相关的重载的方法。底层使用到了native方法实现。
  */
 public class LockSupportDemo {
     public static void main(String[] args){
 
+//        parknanos_demo0();
+        parknanos_demo2();
+    }
+
+    public static void parknanos_demo0(){
         Thread t = new Thread(new Runnable() {
             public void run() {
                 LockSupport.parkNanos(Long.MAX_VALUE);
@@ -18,6 +23,18 @@ public class LockSupportDemo {
 
         t.start();
         LockSupport.unpark(t);
+    }
+
+    public static void parknanos_demo2(){
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                LockSupport.parkNanos(1000000);
+                //interrupted? false.  LockSupport 并不会设置线程的终端标志
+                System.out.println(Thread.currentThread().getName()+" interrupted? "+Thread.interrupted());
+            }
+        });
+
+        t.start();
     }
 
 }
